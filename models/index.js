7d84ded34c43d754e23748d8f484b20e2e1ba496
Sarah Prantis <sprantis@gmail.com>
2022-11-14 16:58:09 -0500

    initial heroku deployment

---
+++ models/index.js
@@ -0,0 +1,23 @@
+// const Book = require('./Book');
+// const Genre = require('./Genre');
+// const Library = require('./Library');
+// const Tags = require('./Tags');
+// const Post = require('./Post');
+// const User = require('./User');
+// const Tag = require('./Tag');
+// const Application = require('./Application');
+const Student = require('./Student');
+const Course = require('./Course');
+
+
+module.exports = { 
+    // Book, 
+    // Genre, 
+    // Library, 
+    // Tags, 
+    // Post, 
+    // User, 
+    // Application, 
+    // Tag, 
+    Student, 
+    Course };
